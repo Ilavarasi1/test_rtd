f509687d9d5b73deddb4c8ad5fc8aa254c4ab813
Ilavarasi1 <sheethal.jan@gmail.com>
2019-01-10 10:08:03 +0000

    INstall nbgrader

--- notebooks/bashfile.bash
+++ notebooks/bashfile.bash
@@ -3,4 +3,9 @@ cd /home/jovyan/work
 export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
 
+pip install nbgrader
+jupyter nbextension install --sys-prefix --py nbgrader --overwrite
+jupyter nbextension enable --sys-prefix --py nbgrader
+jupyter serverextension enable --sys-prefix --py nbgrader
+
 git clone  https://github.com/Ilavarasi1/test_rtd_whatever.git
