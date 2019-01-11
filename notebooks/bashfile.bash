7cd28b2846f275a5e5284f24afc00c584a3e74bf
Ilavarasi1 <sheethal.jan@gmail.com>
2019-01-11 10:13:53 +0000

    some

--- notebooks/bashfile.bash
+++ notebooks/bashfile.bash
@@ -10,10 +10,15 @@ cd /home/jovyan/work
 export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
 
+git clone  https://github.com/Ilavarasi1/test_rtd_whatever.git
+
+cp /home/jovyan/work/test_rtd_whatever/nbgrader_config.py /home/jovyan
+
 
 pip install nbgrader
 jupyter nbextension install --sys-prefix --py nbgrader --overwrite
 jupyter nbextension enable --sys-prefix --py nbgrader
 jupyter serverextension enable --sys-prefix --py nbgrader
 
-git clone  https://github.com/Ilavarasi1/test_rtd_whatever.git
+
+#git clone  https://github.com/Ilavarasi1/test_rtd_whatever.git
